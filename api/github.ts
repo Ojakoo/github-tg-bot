faa2dfdb91f367dadd1961f4ae6ac2dfaea5aff9
Ojakoo <ilari.ojakorpi@gmail.com>
2023-04-14 04:22:42 +0300

    quick handlers for pulls and issues

--- api/github.ts
+++ api/github.ts
@@ -10,13 +10,47 @@ const webhooks = new Webhooks({
   secret: chat_id,
 });
 
-webhooks.on("issues", async ({ id, name, payload }) => {
-  await bot.telegram.sendMessage(chat_id, "jep");
+webhooks.on("pull_request", async ({ payload }) => {
+  if (payload.action === "opened") {
+    await bot.telegram.sendMessage(
+      chat_id,
+      `${payload.sender.login} opened a pull request [${payload.pull_request.title}](${payload.pull_request.html_url})`,
+      { parse_mode: "Markdown" }
+    );
+  } else if (payload.action === "closed") {
+    await bot.telegram.sendMessage(
+      chat_id,
+      `${payload.sender.login} closed a pull request [${payload.pull_request.title}](${payload.pull_request.html_url})`,
+      { parse_mode: "Markdown" }
+    );
+  }
+});
+
+webhooks.on("issues", async ({ payload }) => {
+  if (payload.action === "opened") {
+    await bot.telegram.sendMessage(
+      chat_id,
+      `${payload.sender.login} opened a new issue [${payload.issue.title}](${payload.issue.html_url})`,
+      { parse_mode: "Markdown" }
+    );
+  } else if (payload.action === "closed") {
+    await bot.telegram.sendMessage(
+      chat_id,
+      `${payload.sender.login} closed an issue [${payload.issue.title}](${payload.issue.html_url})`,
+      { parse_mode: "Markdown" }
+    );
+  } else if (payload.action === "reopened") {
+    await bot.telegram.sendMessage(
+      chat_id,
+      `${payload.sender.login} reopened an issue [${payload.issue.title}](${payload.issue.html_url})`,
+      { parse_mode: "Markdown" }
+    );
+  }
 });
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   try {
-    webhooks.receive({
+    await webhooks.receive({
       id: req.headers["x-github-delivery"],
       name: req.headers["x-github-event"],
       payload: req.body,
